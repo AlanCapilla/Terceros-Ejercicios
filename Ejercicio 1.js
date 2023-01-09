08728482a25819e6215111b0b2b1bfd02d1e291b
AlanCapilla <118011904+AlanCapilla@users.noreply.github.com>
2023-01-09 03:13:19 -0600

    Add files via upload

---
+++ Ejercicio 1.js
@@ -0,0 +1,16 @@
+//Ejercicio 1
+let limitador = parseInt(prompt("ingresa el numero de numeros que quieres ingresar"));
+var numeros = new Array(limitador);
+for(var i = 0; i < numeros.length; i++){
+    let numero = parseInt(prompt("Dame numero")); 
+    numeros[i] = numero;
+}
+
+let x = parseInt(prompt("Ingresa valor para comparar"));
+var mayor = 0;
+for(i = 0; i < numeros.length; i++){
+    if(numeros[i] > x){
+        mayor++;
+    }
+}
+console.log("De tu lista,", mayor,"valores son mayor a ",x);
